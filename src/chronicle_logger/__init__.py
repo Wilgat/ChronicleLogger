db71cbf9189c6ef41c5144e9b3ca70fe70042e5b
Wilgat Wong <wilgat.wong@gmail.com>
2025-12-22 20:45:03 +0800

    Update __init__

--- src/chronicle_logger/__init__.py
+++ src/chronicle_logger/__init__.py
@@ -1,5 +1,6 @@
 # logged_example/__init__.py
 from .Suroot import _Suroot
+from .ChronicleLogger import ChronicleLogger
 
-__all__ = ['StateLogic']
-__version__ = "0.1.2"
+__all__ = ['ChronicleLogger']
+__version__ = "0.1.3"
